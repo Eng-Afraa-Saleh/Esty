8f291cc2a577f495cff9afb2b3c9fe3cd89a3a40
Eng-Afraa-Saleh <eng.afraa.saleh@gmail.com>
2026-01-26 01:48:10 +0200

    first commit

--- src/components/sharedComponents/Category.tsx
+++ src/components/sharedComponents/Category.tsx
@@ -1,3 +1,4 @@
+import category from "/assets/icons/category.svg";
 // Type for category props
 interface CategoryProps {
     subTitle:string,
@@ -7,7 +8,7 @@ interface CategoryProps {
 function Category({subTitle,title}:CategoryProps) {
     return (
         <div className="gap-1 md:gap-1.5 xl:gap-2 ">
-            <span className="flex text-Grey-60 gap-0.5 md:gap-1 xl:gap-1.5 text-xs md:text-sm xl:text-lg font-medium items-center urbanist"><img src="/assets/icons/category.svg" alt="" />{subTitle}</span>
+            <span className="flex text-Grey-60 gap-0.5 md:gap-1 xl:gap-1.5 text-xs md:text-sm xl:text-lg font-medium items-center urbanist"><img src={category} alt="" />{subTitle}</span>
             <p className="font-medium text-sm md:text-base xl:text-xl text-White urbanist">{title}</p>
         </div>
     )
